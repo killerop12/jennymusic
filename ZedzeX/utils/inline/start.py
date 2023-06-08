64abc90ecc81b5cf9fa5cae54ee64b92fe94e948
Jenny x pro <123347744+jennyxpro21@users.noreply.github.com>
2023-06-08 11:46:28 +0530

    Update start.py

--- ZedzeX/utils/inline/start.py
+++ ZedzeX/utils/inline/start.py
@@ -36,7 +36,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✯ 𝗢𝘄𝗻𝗲𝗿 ✯", url=f"https://t.me/katil_your_dad"
+                text="✯ 𝗢𝘄𝗻𝗲𝗿 ✯", url=f"https://t.me/jenny_x_01"
             ),
             InlineKeyboardButton(
                 text="✯ 𝗛𝗲𝗹𝗽 ✯", callback_data="settings_back_helper"
@@ -47,13 +47,13 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text="✯ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ✯", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="✯ 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 ✯", url=f"https://t.me/katil_bots",
+                text="✯ 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 ✯", url=f"https://t.me/KILLER_NETWRK",
             )
         ],
         [
             InlineKeyboardButton(
                 text="🌱ƨσʋяcɛ🌱",
-                url=f"https://github.com/team-katil/zedzemusic",
+                url=f"https://github.com/jennyxpro21/jennymusic",
             )
         ],
      ]
