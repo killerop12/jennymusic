a10b3ca4fa5f881403c9238221b0121f7fc2a325
Jenny x pro <123347744+jennyxpro21@users.noreply.github.com>
2023-06-23 20:12:14 +0530

    Update start.py

--- ZedzeX/utils/inline/start.py
+++ ZedzeX/utils/inline/start.py
@@ -9,17 +9,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✯ 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 ✯",
+                text="🥀 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🦋",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="✯ 𝗛𝗲𝗹𝗽 ✯",
+                text="🥀 𝗛𝗲𝗹𝗽 🦋",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="✯ 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀 ✯", callback_data="settings_helper"
+                text="🥀 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀 🦋", callback_data="settings_helper"
             ),
         ],
      ]
@@ -30,29 +30,29 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✯ 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 ✯",
+                text="🥀 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🦋",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="✯ 𝗢𝘄𝗻𝗲𝗿 ✯", url=f"https://t.me/jenny_x_01"
+                text="🥀 𝗢𝘄𝗻𝗲𝗿 🦋", url=f"https://t.me/jenny_x_01"
             ),
             InlineKeyboardButton(
-                text="✯ 𝗛𝗲𝗹𝗽 ✯", callback_data="settings_back_helper"
+                text="🥀 𝗛𝗲𝗹𝗽 🦋", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="✯ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ✯", url=f"https://t.me/jenny_support",
+                text="🥀 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 🦋", url=f"https://t.me/jenny_support",
             ),
             InlineKeyboardButton(
-                text="✯ 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 ✯", url=f"https://t.me/introverthuvaii",
+                text="🥀 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🦋", url=f"https://t.me/introverthuvaii",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌱ƨσʋяcɛ🌱",
+                text="🥀 𝘾𝙃𝘼𝙏𝙏𝙄𝙉𝙂 🦋",
                 url=f"https://github.com/jennyxpro21/jennymusic",
             )
         ],
